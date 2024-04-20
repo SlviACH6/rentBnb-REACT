00d5534a15f2241bf840d2a7f0d504a58b47dca7
SlviACH6 <146087759+SlviACH6@users.noreply.github.com>
2024-04-20 10:56:42 +0200

    house card updates API

--- src/components/HouseCard.jsx
+++ src/components/HouseCard.jsx
@@ -3,39 +3,25 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStar } from '@fortawesome/free-solid-svg-icons'
 import { faCommentDots } from '@fortawesome/free-regular-svg-icons'
 
-function HouseCard(props) {
-  const { isListing } = props
-  const { isbooking } = props
-  const {
-    house_id,
-    location,
-    rooms,
-    bathrooms,
-    price,
-    rating,
-    reviews,
-    reviews_count,
-    photo,
-    checkIn,
-    checkOut,
-    totalNights,
-    totalPrice
-  } = props.house
-
+function HouseCard({ house, booking, listing }) {
   return (
-    <Link to={`/houses/${house_id}`}>
+    <Link to={`/houses/${house.house_id}`}>
       <div className="block border rounded-md">
-        <img src={photo} alt="House image" className="rounded-t" />
+        <img
+          src={house.house_photourl}
+          alt="House image"
+          className="rounded-t"
+        />
         <div className="p-3">
           {/* merge starts here */}
-          <h5 className="font-bold">{location}</h5>
+          <h5 className="font-bold">{house.location}</h5>
           <span className="text-sm">
-            {rooms} rooms • {bathrooms} bathrooms
+            {house.bedrooms} Rooms • {house.bathrooms} Bathrooms
           </span>
-          <h6 className="font-bold py-2">${price}</h6>
+          <h6 className="font-bold py-2">${house.price_night}</h6>
           <div className="flex justify-between">
             <div className="flex">
-              {[...new Array(rating)].map((i, index) => (
+              {[...new Array(house.star_rating)].map((i, index) => (
                 <FontAwesomeIcon
                   key={index}
                   icon={faStar}
@@ -48,11 +34,11 @@ function HouseCard(props) {
                 icon={faCommentDots}
                 className="text-[#94A3B8] mr-1"
               />
-              {reviews || reviews_count}
+              {house.reviews || house.reviews_count}
             </p>
           </div>
           {/* if is Listing page */}
-          {isListing && (
+          {listing && (
             <div className="flex justify-start gap-2 mt-2">
               <button
                 type="submit"
@@ -70,14 +56,14 @@ function HouseCard(props) {
           )}
 
           {/* if is booking page */}
-          {isbooking ? (
+          {booking ? (
             <div className=" flex-col justify-center  pt-2 pb-2 bg-green-100 border rounded-lg">
               <div className=" text-center">
                 {' '}
-                {checkIn} - {checkOut}
+                {booking.check_in} - {booking.check_out}
               </div>
               <div className=" text-center font-bold">
-                {totalNights} Nights = ${totalPrice}
+                {booking.totalNights} Nights = ${booking.totalPrice}
               </div>
             </div>
           ) : null}
